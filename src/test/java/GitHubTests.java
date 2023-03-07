5a3b4682693b12ccce24e6d43e8d4bb0d5a741aa
Прокопьев Дмитрий Викторович (4080425) <prokopevd@dev.vtb.ru>
2023-03-07 15:19:27 +0400

    fix

--- src/test/java/GitHubTests.java
+++ src/test/java/GitHubTests.java
@@ -37,7 +37,6 @@ public class GitHubTests {
 
 //        Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
         $("#wiki-pages-filter").setValue("SoftAssertions");
-        sleep(600);
         $("[data-filterable-for=wiki-pages-filter]").shouldHave(text("SoftAssertions"));
 
 //        Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
